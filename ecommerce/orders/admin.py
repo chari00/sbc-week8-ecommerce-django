4a3a83f55150c3f12146006c9580b5bca8c94c6a
chari00 <macharitocruzuk@gmail.com>
2024-11-10 23:57:47 +0000

    modify templates in settings.py, html files and  admin ui.

--- ecommerce/orders/admin.py
+++ ecommerce/orders/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Order  # Replace with your model name
+from orders.models import Order  # Replace with your model name
 
-admin.site.register(Order)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'product_name', 'quantity', 'order_date')
+
+admin.site.register(Order, PostAdmin)
