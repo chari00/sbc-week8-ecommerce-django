1673f59378366c7de1622b06501765109ecc944a
chari00 <macharitocruzuk@gmail.com>
2024-11-10 20:28:11 +0000

    modify settings.py and create models.py for orders.

--- ecommerce/orders/models.py
+++ ecommerce/orders/models.py
@@ -1,3 +1,21 @@
+from django import forms
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    product_name = models.TextField()
+    quantity = models.IntegerField()
+    order_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+    #     return self.title
+        return f"{self.customer_name} - {self.product_name}"
+
+
+
+class PostForm(forms.ModelForm):
+    # Meta class is used to specify the model and fields we want to include in the form.
+    class Meta:
+        model = Order  # Link the form to the Order model.
+        fields = ('customer_name', 'product_name', 'quantity', 'order_date')  # Specify the fields we want in the form: 'title' and 'description'.
